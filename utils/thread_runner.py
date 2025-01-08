7247b30904d8b5f33658a655bcde20f3a1135cd1
ALi3nW3rX <alienwurx@gmail.com>
2025-01-08 18:28:32 -0500

    updates

--- utils/thread_runner.py
+++ utils/thread_runner.py
@@ -2,12 +2,7 @@ import threading
 from utils.color_logger import log_info, log_success, log_error
 
 def run_in_threads(functions_with_args):
-    """
-    Runs a list of functions with their arguments in parallel using threading.
-    
-    :param functions_with_args: List of tuples where each tuple contains a function and its arguments.
-    Example: [(func1, (arg1, arg2)), (func2, (arg1,))]
-    """
+
     threads = []
 
     for func, args in functions_with_args:
